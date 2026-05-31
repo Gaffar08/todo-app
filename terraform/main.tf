4ca4d664e368405e57575db2c4d38afe1bf77297
Gaffar08 <gaffarkhan2879@gmail.com>
2026-05-31 14:28:02 +0530

    Added ArgoCD application manifest

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,6 @@
+# -----------------------------
+# VPC
+# -----------------------------
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -8,6 +11,9 @@ resource "aws_vpc" "main" {
   }
 }
 
+# -----------------------------
+# PUBLIC SUBNET 1
+# -----------------------------
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -15,10 +21,31 @@ resource "aws_subnet" "public_subnet_1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "todo-public-subnet-1"
+
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
+# -----------------------------
+# PUBLIC SUBNET 2
+# -----------------------------
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "todo-public-subnet-2"
+
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+# -----------------------------
+# INTERNET GATEWAY
+# -----------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -27,6 +54,9 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# -----------------------------
+# ROUTE TABLE
+# -----------------------------
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -36,11 +66,143 @@ resource "aws_route_table" "public_rt" {
   }
 
   tags = {
-    Name = "public-route-table"
+    Name = "todo-public-route-table"
   }
 }
 
+# -----------------------------
+# ROUTE TABLE ASSOCIATION 1
+# -----------------------------
 resource "aws_route_table_association" "public_assoc_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+# -----------------------------
+# ROUTE TABLE ASSOCIATION 2
+# -----------------------------
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# -----------------------------
+# EKS IAM ROLE
+# -----------------------------
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "todo-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# -----------------------------
+# EKS NODE ROLE
+# -----------------------------
+resource "aws_iam_role" "eks_node_role" {
+  name = "todo-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "container_registry_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# -----------------------------
+# EKS CLUSTER
+# -----------------------------
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_subnet_1.id,
+      aws_subnet.public_subnet_2.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name = "todo-eks-cluster"
+  }
+}
+
+# -----------------------------
+# EKS NODE GROUP
+# -----------------------------
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "todo-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  instance_types = ["t3.small"]
+
+  capacity_type = "ON_DEMAND"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.container_registry_policy
+  ]
+
+  tags = {
+    Name = "todo-node-group"
+  }
 }
