ada557e5b270f82c5e24134004a8f3acd216d66b
Gaffar08 <gaffarkhan2879@gmail.com>
2026-06-24 20:03:50 +0530

    Use IRSA instead of static AWS credentials

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,12 +2,9 @@ require("dotenv").config();
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+  region: process.env.AWS_REGION
 });
 
 const docClient = DynamoDBDocumentClient.from(client);
